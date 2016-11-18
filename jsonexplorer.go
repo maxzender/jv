c5656c33ca15749991e33a8e4fcaad226fcf2622
Max Zender <mail@maxzender.de>
2016-11-18 17:34:16 +0100

    Handle cursor positioning when resizing the terminal window

--- jsonexplorer.go
+++ jsonexplorer.go
@@ -2,6 +2,54 @@ package main
 
 import "github.com/nsf/termbox-go"
 
+type Window struct {
+	Width, Height    int
+	CursorX, CursorY int
+}
+
+func NewWindow(w, h int) *Window {
+	return &Window{Width: w, Height: h}
+}
+
+func (w *Window) MoveCursor(x, y int) {
+	newX := w.CursorX + x
+	if newX < 0 {
+		newX = 0
+	} else if newX >= w.Width {
+		newX = w.CursorX
+	}
+	newY := w.CursorY + y
+	if newY < 0 {
+		newY = 0
+	} else if newY >= w.Height {
+		newY = w.CursorY
+	}
+
+	w.CursorX, w.CursorY = newX, newY
+}
+
+func (w *Window) Resize(width, height int) {
+	w.Width = width
+	w.Height = height
+
+	if w.CursorX > width {
+		w.CursorX = width
+	}
+	if w.CursorY > height {
+		w.CursorY = height
+	}
+}
+
+var (
+	window *Window
+	keyMap = map[rune]func(){
+		'h': func() { window.MoveCursor(-1, 0) },
+		'j': func() { window.MoveCursor(0, +1) },
+		'k': func() { window.MoveCursor(0, -1) },
+		'l': func() { window.MoveCursor(+1, 0) },
+	}
+)
+
 func main() {
 	err := termbox.Init()
 	if err != nil {
@@ -9,34 +57,27 @@ func main() {
 	}
 	defer termbox.Close()
 
-	eventCh := make(chan termbox.Event)
-	draw := drawer()
+	window = NewWindow(termbox.Size())
 
-	go pollEvents(eventCh)
-
-	for event := range eventCh {
-		draw(event.Ch)
-	}
-}
-
-func pollEvents(eventCh chan termbox.Event) {
 	for {
 		switch e := termbox.PollEvent(); e.Type {
 		case termbox.EventKey:
 			if e.Ch == 'q' || e.Key == termbox.KeyCtrlC {
-				close(eventCh)
+				return
 			} else {
-				eventCh <- e
+				handleKeyPress(e)
 			}
+		case termbox.EventResize:
+			window.Resize(e.Width, e.Height)
 		}
+		termbox.SetCursor(window.CursorX, window.CursorY)
+		termbox.Flush()
 	}
 }
 
-func drawer() func(rune) {
-	i := 0
-	return func(r rune) {
-		termbox.SetCell(i, 0, r, termbox.ColorWhite, termbox.ColorDefault)
-		i++
-		termbox.Flush()
+func handleKeyPress(event termbox.Event) {
+	handler, ok := keyMap[event.Ch]
+	if ok {
+		handler()
 	}
 }
